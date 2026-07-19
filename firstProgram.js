a402cb813b391c99ca9e7b37b184ee4be83cb128
NancyRoyan <nancyroyan@gmail.com>
2026-07-19 13:10:56 +0530

    Added day 1 and day2 assignments

---
+++ firstProgram.js
@@ -0,0 +1,14 @@
+for(let i=0;i<10;i++){console.log('Nancy')}
+console.log('Hello World')
+var firstName = 'Nancy Maria'
+var companyName = 'Natwest'
+var mobileNumber = 8678775566
+var isAutomation = true
+var hasPlaywright
+console.log(firstName)
+console.log(companyName)
+console.log(mobileNumber)
+console.log(isAutomation)
+console.log(hasPlaywright)
+console.log('TotalSal=' + (salary + salary))
+
